c655c63b5f86f0b044b05b76ceeb0a032e3cd6cf
Jadir Arruda Neves <jadir.arruda.neves@gmail.com>
2025-07-31 00:38:03 +0000

    exercicio numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,18 @@
 data_nascimento = '05-10-1976'
-idade_numerica = 46
 altura = 1.74
-
+idade_numerica = 46
 # Descubra o tipo de dado de cada variável acima
-
+ data_nascimento = '05-10-1976'
+print(type(data_nascimento))
+class 'str'>
+idade_numerica = 46
+print(type(idade_numerica))
+class 'float'>
 
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+print('paloma'*2)
+palomapaloma
 # Realize uma operação entre dados do tipo inteiro e float
+soma =2+1.74
+print(2+1.74)
+3.74
